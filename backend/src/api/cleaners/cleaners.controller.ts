d30e5d54581def7f0bc5e03daaa655d8bb8c4764
waghD <33834105+waghD@users.noreply.github.com>
2020-05-28 14:01:14 +0200

    implement cleaner api

--- backend/src/api/cleaners/cleaners.controller.ts
+++ backend/src/api/cleaners/cleaners.controller.ts
@@ -9,36 +9,74 @@ import {
   NotImplementedException,
   UsePipes,
   ValidationPipe,
+  InternalServerErrorException,
 } from '@nestjs/common';
 import { Cleaner, UpdateCleaner, CreateCleaner } from './cleaner.dto';
 import { ApiTags } from '@nestjs/swagger';
+import { CleanersService } from './cleaners.service';
+import { DatabaseService } from 'src/database/database.service';
 
 @ApiTags('Cleaners')
 @UsePipes(ValidationPipe)
 @Controller('cleaners')
 export class CleanersController {
+  constructor(private service: CleanersService, private db: DatabaseService) {}
+
   @Get()
-  getAll(): Cleaner[] {
-    throw new NotImplementedException();
+  async getAll(): Promise<Cleaner[]> {
+    try {
+      const cleaners = await this.db.getCleaners();
+      return cleaners;
+    } catch (e) {
+      console.error(e);
+      throw new InternalServerErrorException();
+    }
   }
 
   @Get(':id')
-  get(@Param('id') id: number): Cleaner {
-    throw new NotImplementedException();
+  async get(@Param('id') id: number): Promise<Cleaner> {
+    try {
+      const cleaner = await this.db.getCleanerById(id);
+      return cleaner;
+    } catch (e) {
+      console.error(e);
+      throw new InternalServerErrorException();
+    }
   }
 
   @Put()
-  create(@Body() cleaner: CreateCleaner): Cleaner {
-    throw new NotImplementedException();
+  async create(@Body() createCleaner: CreateCleaner): Promise<Cleaner> {
+    try {
+      const cleaner = await this.service.createCleaner(createCleaner);
+      return cleaner;
+    } catch (e) {
+      console.error(e);
+      throw new InternalServerErrorException();
+    }
   }
 
   @Post(':id')
-  update(@Param('id') id: number, @Body() cleaner: UpdateCleaner): Cleaner {
-    throw new NotImplementedException();
+  async update(
+    @Param('id') id: number,
+    @Body() UpdateCleaner: UpdateCleaner,
+  ): Promise<Cleaner> {
+    try {
+      const cleaner = await this.service.updateCleaner(id, UpdateCleaner);
+      return cleaner;
+    } catch (e) {
+      console.error(e);
+      throw new InternalServerErrorException();
+    }
   }
 
   @Delete(':id')
-  delete(@Param('id') id: number): Cleaner {
-    throw new NotImplementedException();
+  async delete(@Param('id') id: number): Promise<Cleaner> {
+    try {
+      const cleaners = await this.service.deleteCleaner(id);
+      return cleaners;
+    } catch (e) {
+      console.error(e);
+      throw new InternalServerErrorException();
+    }
   }
 }
--- backend/src/api/cleaners/cleaners.service.ts
+++ backend/src/api/cleaners/cleaners.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { DatabaseService } from 'src/database/database.service';
+import { CreateCleaner, Cleaner, UpdateCleaner } from './cleaner.dto';
 
 @Injectable()
-export class CleanersService {}
+export class CleanersService {
+  constructor(private db: DatabaseService) {}
+
+  async createCleaner(cleaner: CreateCleaner): Promise<Cleaner> {
+    const clean = new Cleaner();
+    clean.name = cleaner.name;
+    clean.surname = cleaner.surname;
+    return this.db.saveCleaner(clean);
+  }
+
+  async updateCleaner(id: number, clean: UpdateCleaner): Promise<Cleaner> {
+    const cleaner = await this.db.getCleanerById(id);
+    if (clean.name) {
+      cleaner.name = clean.name;
+    }
+
+    if (clean.surname) {
+      cleaner.surname = clean.surname;
+    }
+
+    return this.db.saveCleaner(cleaner);
+  }
+
+  async deleteCleaner(id: number): Promise<Cleaner> {
+    const cleaner = await this.db.getCleanerById(id);
+    await this.db.deleteCleaner(id);
+    return cleaner;
+  }
+}
