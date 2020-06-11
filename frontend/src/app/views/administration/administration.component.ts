7b0601ac867565e28cac237c24dc41774f12dc3c
waghD <33834105+waghD@users.noreply.github.com>
2020-06-11 15:51:33 +0200

    add ngSwitch to show different admin components

--- frontend/src/app/views/administration/administration.component.ts
+++ frontend/src/app/views/administration/administration.component.ts
@@ -1,12 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { AdministrationService } from './administration.service';
 
+enum AdminComponentTypes {
+  DOCTOR,
+  HELPER,
+  CLEANER,
+  PATIENT,
+}
+
 @Component({
   selector: 'app-administration',
   templateUrl: './administration.component.html',
   styleUrls: ['./administration.component.css'],
 })
 export class AdministrationComponent implements OnInit {
+  displayComponent: AdminComponentTypes = AdminComponentTypes.DOCTOR;
+
+  // Hack um Enum in HTML Template zu verwenden
+  adminComponentTypes = AdminComponentTypes;
+
   constructor(private adminService: AdministrationService) {}
 
   ngOnInit(): void {}
--- frontend/src/app/views/administration/administration.module.ts
+++ frontend/src/app/views/administration/administration.module.ts
@@ -4,11 +4,21 @@ import { AdministrationComponent } from './administration.component';
 import { Routes, RouterModule } from '@angular/router';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { AdministrationService } from './administration.service';
+import { DoctorAdminComponent } from './components/doctor-admin/doctor-admin.component';
+import { PatientAdminComponent } from './components/patient-admin/patient-admin.component';
+import { HelpersAdminComponent } from './components/helpers-admin/helpers-admin.component';
+import { CleanersAdminComponent } from './components/cleaners-admin/cleaners-admin.component';
 
 const routes: Routes = [{ path: '', component: AdministrationComponent }];
 
 @NgModule({
-  declarations: [AdministrationComponent],
+  declarations: [
+    AdministrationComponent,
+    DoctorAdminComponent,
+    PatientAdminComponent,
+    HelpersAdminComponent,
+    CleanersAdminComponent,
+  ],
   imports: [CommonModule, RouterModule.forChild(routes), SharedModule],
   providers: [AdministrationService],
 })
