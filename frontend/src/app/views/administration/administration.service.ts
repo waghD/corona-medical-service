36c1211a43aeaec5715bfaaf3cacc46202f68ee3
waghD <33834105+waghD@users.noreply.github.com>
2020-06-13 09:12:41 +0200

    move code to administration service

--- frontend/src/app/views/administration/administration.service.ts
+++ frontend/src/app/views/administration/administration.service.ts
@@ -1,7 +1,67 @@
 import { Injectable } from '@angular/core';
 import { ApiConnectionService } from 'src/app/shared/services/api-connection.service';
+import { Observable } from 'rxjs';
+import { Doctor } from 'src/app/shared/models/doctor.model';
+import { Shift } from 'src/app/shared/models/shift.model';
 
 @Injectable()
 export class AdministrationService {
   constructor(private apiConnection: ApiConnectionService) {}
+
+  public getShifts(): Observable<Doctor[]> {
+    const doctors = this.apiConnection.getDoctors();
+    return doctors;
+  }
+
+  public deleteDoc(id: number) {
+    this.apiConnection.deleteDoctor(id).subscribe((doc) => {
+      console.log('Deleted Doctor: ', doc);
+    });
+  }
+
+  public editDoc(id: number, doc: Doctor) {
+    this.apiConnection
+      .editDoctor(id, {
+        name: doc.name,
+        profession: doc.profession,
+        surname: doc.surname,
+      })
+      .subscribe((updateDoc) => {
+        console.log('Updated Doc: ', updateDoc);
+      });
+  }
+
+  public createDoc(doc: Doctor) {
+    const newDoc = new Doctor({
+      id: 9,
+      name: doc.name,
+      profession: doc.profession,
+      surname: doc.surname,
+    });
+    this.apiConnection.createDoctor(newDoc).subscribe((doc) => {
+      console.log('Created Doctor: ', doc);
+    });
+  }
+
+  public createShift(shift: Shift) {
+    const newShift = new Shift({
+      id: -1,
+      from: shift.from.toISOString(),
+      to: shift.from.toISOString(),
+
+      cleaner: null,
+      // hier steh ich jetzt leider an, ich versteh nicht warum hier doc nicht passt..
+      // eigentlich sollten hier null eingaben funktionieren
+      // zur Vereinfachung der Aufgabe würde ich vorschlagen, dass wir:
+      //- nur Tageweise Schichten machen
+      //- Schichten lassen sich nur erstellen, aber nicht löschen
+      // dann sollte die Aufgabe bis Sonntag aufjedenfall schaffbar sein...
+      doc: shift.doc,
+      helper: null,
+      station: null,
+    });
+    this.apiConnection.createShift(newShift).subscribe((shift) => {
+      console.log('Created Shift: ', shift);
+    });
+  }
 }
--- frontend/src/app/views/administration/components/doctor-admin/doctor-admin.component.ts
+++ frontend/src/app/views/administration/components/doctor-admin/doctor-admin.component.ts
@@ -2,72 +2,68 @@ import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { AdministrationService } from '../../administration.service';
 import { MatTableDataSource } from '@angular/material/table';
 import { Doctor } from 'src/app/shared/models/doctor.model';
-import { DoctorAdminService } from './doctor-admin.service';
-import { ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import {MatDialogModule} from "@angular/material/dialog";
 import { DialogBoxComponent } from './dialog-box/dialog-box.component';
 import { Shift } from 'src/app/shared/models/shift.model';
-
 
 @Component({
   selector: 'app-doctor-admin',
   templateUrl: './doctor-admin.component.html',
   styleUrls: ['./doctor-admin.component.css'],
-  providers: [DoctorAdminService]
 })
 export class DoctorAdminComponent implements OnInit {
-  displayedColumns = ['id', 'name', 'surname', 'profession','actions'];
+  displayedColumns = ['id', 'name', 'surname', 'profession', 'actions'];
   doctors: Doctor[] = [];
   doctorsTableDataSource = new MatTableDataSource<Doctor>();
-  
-  constructor(private doctorAdminService: DoctorAdminService, public dialog: MatDialog, private changeDetectorRefs: ChangeDetectorRef) {}
+
+  constructor(
+    private doctorAdminService: AdministrationService,
+    public dialog: MatDialog,
+    private changeDetectorRefs: ChangeDetectorRef
+  ) {}
 
   ngOnInit() {
     this.refresh();
     // refresh funzt leider noch nicht wie es soll
     // Lösungsidee: Redirect zur Homepage?
   }
-  
-  
 
-  openDialog(action,obj) {
+  openDialog(action, obj) {
     obj.action = action;
     const dialogRef = this.dialog.open(DialogBoxComponent, {
       width: '250px',
-      data:obj
+      data: obj,
     });
 
-    dialogRef.afterClosed().subscribe(result => {
-      if(result.event == 'neuer Arzt'){
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result.event == 'neuer Arzt') {
         this.addRowData(result.data);
-      }else if(result.event == 'bearbeiten'){
+      } else if (result.event == 'bearbeiten') {
         this.updateRowData(result.data);
-      }else if(result.event == 'löschen'){
+      } else if (result.event == 'löschen') {
         this.deleteRowData(result.data);
-      }else if(result.event == 'neue Schicht'){
+      } else if (result.event == 'neue Schicht') {
         this.newShift(result.data);
       }
     });
   }
 
-  addRowData(row_obj){
+  addRowData(row_obj) {
     console.log('add row');
     this.doctorAdminService.createDoc(row_obj);
     this.refresh();
-
   }
-  updateRowData(row_obj){
+  updateRowData(row_obj) {
     console.log('update');
-    this.doctorAdminService.editDoc(row_obj.id,row_obj);
+    this.doctorAdminService.editDoc(row_obj.id, row_obj);
     this.refresh();
   }
-  deleteRowData(row_obj){
+  deleteRowData(row_obj) {
     this.doctorAdminService.deleteDoc(row_obj.id);
     this.refresh();
   }
 
-  newShift(row_obj){
+  newShift(row_obj) {
     console.log('neue Schicht am: ', row_obj.Date);
     console.log('neue Schicht von: ', row_obj.id);
     const newShift = new Shift({
@@ -86,14 +82,11 @@ export class DoctorAdminComponent implements OnInit {
 
   refresh() {
     this.doctorAdminService.getShifts().subscribe((data: Doctor[]) => {
-    console.log('refresh data');
-    this.doctors = data;
-    this.doctorsTableDataSource = new MatTableDataSource<Doctor>();
-    this.doctorsTableDataSource.data = this.doctors;
-    this.changeDetectorRefs.detectChanges();
-    
+      console.log('refresh data');
+      this.doctors = data;
+      this.doctorsTableDataSource = new MatTableDataSource<Doctor>();
+      this.doctorsTableDataSource.data = this.doctors;
+      this.changeDetectorRefs.detectChanges();
     });
-
-
-}
+  }
 }
