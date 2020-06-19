661910f0010159276e235de9644d47bdf7e78696
waghD <33834105+waghD@users.noreply.github.com>
2020-06-19 12:03:26 +0200

    remove test component

--- src/app/shared/components/components.module.ts
+++ src/app/shared/components/components.module.ts
@@ -4,10 +4,9 @@ import { HeaderComponent } from './header/header.component';
 import { MaterialModule } from '../material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { RouterModule } from '@angular/router';
-import { TestcomponentComponent } from './testcomponent/testcomponent.component';
 
 @NgModule({
-  declarations: [HeaderComponent, TestcomponentComponent],
+  declarations: [HeaderComponent],
   imports: [CommonModule, MaterialModule, FlexLayoutModule, RouterModule],
   exports: [HeaderComponent, MaterialModule],
 })
